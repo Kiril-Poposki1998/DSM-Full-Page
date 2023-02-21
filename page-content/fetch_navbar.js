247444c6ac9fb96752bd40ea90a42d990a078b39
Kiril Poposki <kirilpoposki@hotmail.com>
2023-02-21 21:28:17 +0100

    fixing fetch for navbar

--- page-content/fetch_navbar.js
+++ page-content/fetch_navbar.js
@@ -3,7 +3,8 @@
     switch (response.status) {
         case 200:
             var template = await response.text();
-            console.log(template);
+            var div_navbar = document.getElementById("navbar");
+            div_navbar.innerHTML = template;
             break;
         case 404:
             console.log('Navbar Not found');
