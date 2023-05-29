7b0ac1ed580ca93dc6a24851a4533ab852301f76
Kiril Poposki <kirilpoposki@hotmail.com>
2023-05-29 23:13:48 +0200

    add fetch for footer

--- page-content/fetch_navbar.js
+++ page-content/fetch_navbar.js
@@ -11,3 +11,17 @@
             break;
     }
 })();
+
+(async () => {
+    var response = await fetch('http://dsm.local/footer');
+    switch (response.status) {
+        case 200:
+            var template = await response.text();
+            var div_navbar = document.getElementById("footer");
+            div_navbar.innerHTML = template;
+            break;
+        case 404:
+            console.log('Footer Not found');
+            break;
+    }
+})();
